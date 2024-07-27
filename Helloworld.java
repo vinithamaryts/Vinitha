6dedf0afcc0c3a0ef05d21b140a37eb5723b6b56
vinithamaryts <vinithamaryts@gmail.com>
2024-07-27 13:10:04 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+import java.time.Clock;
+
+public class Helloworld {
+    public static void main(String[] args) {
+
+        System.out.println ("hi");
+        System.out.println("vinitha");
+
+    }
+
+
+
+}
